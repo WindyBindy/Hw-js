380970ca86cb228bae35d95c7aa6c10b7c796d5f
WindyBindy <134945382+WindyBindy@users.noreply.github.com>
2025-04-03 14:58:22 +0200

    nice

---
+++ script.js
@@ -0,0 +1,34 @@
+const celsius = 50
+const fahrenheit = celsius * 1.8 + 32
+console.log(fahrenheit)
+const daysInMonth = 31
+const hoursInMonth = 31 * 24
+const minutesInMonth = hoursInMonth * 60
+console.log('hours in month:' + minutesInMonth)
+console.log('minutes in month' + hoursInMonth)
+const health = 100
+const energy = 100
+console.log('palyers health:' + health)
+console.log('palyers energy:' + energy)
+const totalPrice = 23
+const discount = (totalPrice / 100) * 10
+const discountedPrice = (totalPrice / 100) * 10
+console.log(discountedPrice)
+const floatNumber = 12.78
+const roundedDown = Math.floor(floatNumber)
+console.log(roundedDown)
+const floatString = '45.67'
+const parsedFloat = parseFloat(floatString)
+console.log(parsedFloat + ' typeof: ' + typeof parsedFloat)
+const intString = '123'
+const parsedInt = parseInt(intString)
+console.log(parsedInt)
+const number = 12
+const sqrtNumber = Math.sqrt(number)
+console.log(sqrtNumber)
+const integer = 42
+const stringNumber = '256'
+const convertedInt = parseInt(stringNumber)
+console.log(convertedInt)
+const convertedString = toString(integer)
+console.log(convertedString)
